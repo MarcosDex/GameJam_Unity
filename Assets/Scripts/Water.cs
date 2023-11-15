1c593c99cf81afe969157f60207355f8d2b116c9
ISRAEL FONTES <israeldefontes@gmaile.com>
2023-11-15 15:16:07 -0300

    gerar fonte de agua

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -10,20 +10,23 @@ public class Water : MonoBehaviour
         SpawnRandomCube();
     }
 
-    void SpawnRandomCube()
+    async void SpawnRandomCube()
     {
         
         
             // Tamanho do terrain
-            float terrainWidth = 50f;
+            float terrainWidth = 49f;
+            float MaxX = 49f; // colocando o maxio do eixo x para ser gerado o maior limite do mapa 
+            float MinX = 1.0f; // colocando o minimo do eixo x para ser gerado o menor limite do mapa 
             float terrainLength = 50f;
-            float terrainHeight = 10f;
+            float terrainHeight = -0.4f;
+
 
             // Gera uma posição aleatória no espaço dentro das dimensões do terrain
             Vector3 spawnPosition = new Vector3(
-                Random.Range(-terrainWidth / 2f, terrainWidth / 2f),  // Coordenada X
+                Random.Range(MaxX, MinX),  // Coordenada X
                 Random.Range(0f, terrainHeight),                      // Coordenada Y
-                Random.Range(-terrainLength / 2f, terrainLength / 2f) // Coordenada Z
+                Random.Range(terrainLength / 2f, terrainLength / 2f) // Coordenada Z
             );
 
             // Instancia o cubo na posição gerada
