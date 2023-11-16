71e37a1af0d6230b49245691139489e40efa0b60
MarcosDex <marquinhospbongame@gmail.com>
2023-11-16 16:09:49 -0300

    Adicionado as coisinhas de cam e animações

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -1,54 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Cinemachine;
 public class Camera : MonoBehaviour
 {
-    public Transform alvo; // Referência ao transform do personagem
-    public float sensibilidadeMouse = 2f;
-    public float rotacaoMinimaY = -80f; // Limite mínimo de rotação vertical
-    public float rotacaoMaximaY = 80f;  // Limite máximo de rotação vertical
+    [SerializeField] private AxisState xAxis;
+    [SerializeField] private AxisState yAxis;
+    [SerializeField] private Transform olhando;
 
-    private Vector3 offset; // Armazena a posição inicial relativa à câmera
-
-    void Start()
+    private void Update()
     {
-        if (alvo == null)
-        {
-            Debug.LogError("Por favor, atribua o objeto do personagem à variável 'alvo' no Inspector.");
-            return;
-        }
+        xAxis.Update(Time.deltaTime);
+        yAxis.Update(Time.deltaTime); 
 
-        // Calcula e armazena a posição inicial relativa à câmera
-        offset = transform.position - alvo.position;
-    }
+        olhando.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, 0);
 
-    void Update()
-    {
-        if (alvo == null)
-        {
-            Debug.LogError("Por favor, atribua o objeto do personagem à variável 'alvo' no Inspector.");
-            return;
-        }
-
-        // Obtém o movimento do mouse no eixo horizontal
-        float movimentoMouseX = Input.GetAxis("Mouse X");
-
-        // Rotaciona a câmera apenas em torno do eixo Y
-        transform.RotateAround(alvo.position, Vector3.up, movimentoMouseX * sensibilidadeMouse);
-
-        // Obtém a rotação vertical atual
-        Vector3 eulerRotation = transform.rotation.eulerAngles;
-
-        // Calcula a nova rotação vertical com base no movimento do mouse
-        float rotacaoY = -Input.GetAxis("Mouse Y") * sensibilidadeMouse;
-        eulerRotation.x = Mathf.Clamp(eulerRotation.x + rotacaoY, rotacaoMinimaY, rotacaoMaximaY);
-
-        // Aplica a rotação ajustada à câmera
-        transform.rotation = Quaternion.Euler(eulerRotation);
-
-        // Mantém a posição relativa da câmera em relação ao personagem
-        transform.position = alvo.position + offset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, xAxis.Value, 0), 5 * Time.deltaTime);
     }
 }
 
